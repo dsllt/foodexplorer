6dfc3be974185c787b2f42532e6de1f363983221
Dayane Sallet <dayanesallet@hotmail.com>
2023-02-03 15:21:05 -0300

    Inclusao da coluna de categoria na tabela de meals

--- api/src/database/knex/migrations/20221219135528_createMeals.js
+++ api/src/database/knex/migrations/20221219135528_createMeals.js
@@ -2,6 +2,7 @@ exports.up = knex => knex.schema.createTable('meals', table => {
   table.increments("id");
   table.text("name");
   table.text("description");
+  table.text("category");
   table.float("price");
   table.text("image");
   table.timestamp("created_at").default(knex.fn.now());
